ec520f87dcfee323f6e67cacaa9696d10227643a
huichiy <yaphuichiy@gmail.com>
2025-01-09 00:54:05 +0800

    update create database if else part
    Able to let user to choose open or not open database:
    - if open, then let user type database name and show database name again
    - if not open, ask user want to open database or not
    - if not input the correct choice, show invalid choice
    
    For not open new database:
    - ask user want to open database that are in local or not, if yes, ask user input the database name
    - if no, show database not open
    - if not the valid choice, output invalid choice
    - if database name is not correct or database not open, show output database is not open

--- main.cpp
+++ main.cpp
@@ -2,13 +2,63 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <string>
 using namespace std;
 
-void
+void createDatabase(string db);
 
-    int
-    main()
+int main()
 {
 
     return 0;
+}
+
+void createDatabase(string db)
+{
+    cout << "Do you want to create new database to store data? (y/n)" << endl;
+    char choice;
+    cin >> choice;
+
+    if (choice == 'y')
+    {
+        cout << "Enter database name: ";
+        cin >> db;
+        cout << "Database created successfully." << endl;
+        cout << "Database name: " << db << endl;
+    }
+
+    else if (choice == 'n')
+    {
+        cout << "Database not created. Do you want to open existing database? (y/n)" << endl;
+        char choice2;
+        cin >> choice2;
+
+        if (choice2 == 'y')
+        {
+            cout << "Enter database name: ";
+            cin >> db;
+            cout << "Database opened successfully." << endl;
+            cout << "Database name: " << db << endl;
+        }
+
+        else if (choice2 == 'n')
+        {
+            cout << "Database not opened." << endl;
+        }
+
+        else if (choice2 != 'y' || 'n')
+        {
+            cout << "Invalid choice. Please enter y or n." << endl;
+        }
+
+        else
+        {
+            cout << "Database not opened. Try check the database name." << endl;
+        }
+    }
+
+    else if (choice != 'y' || 'n')
+    {
+        cout << "Invalid choice. Please enter y or n." << endl;
+    }
 }
