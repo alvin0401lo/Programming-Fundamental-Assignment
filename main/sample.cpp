d0f922442fdbbe8ad61a8c13f238c060900b554a
AH BOON <87453214+alvin0401lo@users.noreply.github.com>
2025-01-08 23:59:26 +0800

    Update sample.cpp
    added some functions

--- main/sample.cpp
+++ main/sample.cpp
@@ -1,116 +1,128 @@
+//newFeature
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <iomanip>
 using namespace std;
 
-void Update();
-void Delete();
-void Count();
 void file_writing();
 void file_reading();
-void Option();
+void Delete();
+void Update();
+void Count();
 
-int id;
-string name, city, state, couwntry, phone, email;
+void secondOption();
+int ctm_id;
+string ctm_name, ctm_city, ctm_state, 
+       ctm_country, ctm_phone, ctm_email;
 
 int main(){
 
-string mode;
+int mode;
 
-cout << "Writing / Reading ?" << endl;
-cin >> mode;
-    
-    if (mode == "Writing")
-    {
-        //writing mode
-        file_writing();
-        file_reading();
-    } 
-    else if ( mode == "Reading")
-    {
-        //reading mode
-        file_reading();
-    } 
-    else 
-    {
-        cout << "error!";
-        return 0;
-    }
+while (true){
+    cout << "1. Write to the file" << endl;
+    cout << "2. Read from a file" << endl;
+    cout << "3. Exit" << endl;
+    cin >> mode;
 
-   
-
-while(true)
-{       cout << endl
-        << "1. Delete" << endl
-        << "2. Update" << endl
-        << "3. Count"  << endl 
-        << "4. Exit" << endl;
-        int option;
-        cin >> option;
-
-        switch(option) 
-     {
+    switch(mode){
         case 1:
-            Delete();
+            file_writing();
+            secondOption();
             break;
 
         case 2:
-            Update();
+            file_reading();
+            secondOption();
             break;
-
+        
         case 3:
-            Count();
-            break;
-        
-        case 4:
-            cout << "Exiting..." << endl;
-            return 0;
-        
+            cout << "Exiting......" << endl;
+            return true;
+
         default:
-            cout << "Invalid option. Try again!" << endl;
+            cout << "Error, please input a valid number." << endl;
             break;
-     }
+    }
+
 }
-
-
+    
 return 0;
 }
 
-void Update(){
-    cout << "Updated!" << endl;
-};//
-void Delete(){
-    cout << "Deleted!" << endl;
-};
-void Count(){
-    cout << "counted!" << endl;
-};
 void file_writing(){
-    ofstream inputfile;
-    inputfile.open("outputfile1.txt");
+    string fileName; 
+    ofstream inputfile; 
+ 
+    cout << "Please enter the file name: " << endl; 
+    cin >> fileName; 
+ 
+    inputfile.open(fileName + ".mdb"); 
+    
+    if (!inputfile) 
+    { 
+        cout << "Error: Could not create file!" << endl; 
+        return; 
+    } 
+    else 
+    { 
+        cout << "File created successfully!" << endl; 
+    } 
 
-    int maxcustomer;
-    cout << "please enter maxcustomer";
-    cin >> maxcustomer;
+    int numCustomer; 
+    cout << "Please enter the number of customers: " << endl; 
+    cin >> numCustomer; 
 
-        for(int id = 1; id <= maxcustomer; id++){
-            
+    // Write the headers with width settings
+    inputfile << left << setw(5) << "ID" << setw(20) << "Name" << setw(20) << "City" 
+               << setw(20) << "State" << setw(20) << "Country" << setw(20) << "Phone" 
+               << setw(30) << "Email" << endl;
 
-            cout << "please enter name" << id << endl;
-            cin >> name;
-            
+    for (int id = 1; id <= numCustomer; id++) 
+    { 
+        cout << "Please enter name " << id << ": " << endl;
+        cin >> ctm_name;
 
-            inputfile << id << ", " << name << endl;
-        };
+        cout << "Please enter city " << id << ": " << endl;
+        cin >> ctm_city;
 
-    inputfile.close();
+        cout << "Please enter state " << id << ": " << endl;
+        cin >> ctm_state;
 
-};
+        cout << "Please enter country " << id << ": " << endl;
+        cin >> ctm_country;
+
+        cout << "Please enter phone " << id << ": " << endl;
+        cin >> ctm_phone;
+
+        cout << "Please enter email " << id << ": " << endl;
+        cin >> ctm_email;
+
+        inputfile << left << setw(5) << id << setw(20) << ctm_name << setw(20) << ctm_city 
+                   << setw(20) << ctm_state << setw(20) << ctm_country << setw(20) << ctm_phone 
+                   << setw(30) << ctm_email << endl; 
+    } 
+
+    inputfile.close(); 
+    cout << "Data written successfully!" << endl;
+
+    // Wait for user to press any key 
+    cout << "Press any key to continue..." << endl; 
+    cin.ignore(); // Ignore the newline character left in the input buffer 
+    cin.get(); // Wait for user to press any key
+}
+
 void file_reading(){
     
-
+    string fileName;
     ifstream infile;
-    infile.open("outputfile1.txt");
+
+    cout << "please enter the file name : " << endl;
+    cin >> fileName;
+
+    infile.open(fileName + ".mdb");
     string line;
 
     if (infile.is_open()) {
@@ -119,8 +131,74 @@ void file_reading(){
         while (getline(infile, line)) {
             cout << line << endl;
         };
+        }else {
+            cout << "Error" << endl;
         }
         
     infile.close();
+    // Wait for user to press any key 
+    cout << "Press any key to continue..." << endl; 
+    cin.ignore(); // Ignore the newline character left in the input buffer 
+    cin.get(); // Wait for user to press any key
 
+};
+
+void secondOption() 
+{ 
+    while(true){
+    cout 
+        << "1. Delete" << endl 
+        << "2. Update" << endl 
+        << "3. Count" << endl 
+        << "4. Exit" << endl;
+
+    int option;
+    cin >> option;
+
+    switch(option)
+        {
+        case 1:
+            Delete();
+            break;
+        case 2:
+            Update();
+            break;
+        case 3:
+            Count();
+            break;
+        case 4:
+            cout << "Exiting......" << endl;
+            return ;
+        default:
+            cout << "Invalid option, please try again!" << endl;
+
+        }
+    }
+}
+
+void Update(){
+    cout << "Updated!" << endl;
+    
+    // Wait for user to press any key 
+    cout << "Press any key to continue..." << endl; 
+    cin.ignore(); // Ignore the newline character left in the input buffer 
+    cin.get(); // Wait for user to press any key
+};
+
+void Delete(){
+    cout << "Deleted!" << endl;
+    
+    // Wait for user to press any key 
+    cout << "Press any key to continue..." << endl; 
+    cin.ignore(); // Ignore the newline character left in the input buffer 
+    cin.get(); // Wait for user to press any key
+};
+
+void Count(){
+    cout << "counted!" << endl;
+    
+    // Wait for user to press any key 
+    cout << "Press any key to continue..." << endl; 
+    cin.ignore(); // Ignore the newline character left in the input buffer 
+    cin.get(); // Wait for user to press any key
 };
